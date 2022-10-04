9565fa498876af6ca21f04da4becd6db4b0c2b42
Ross Mitchell <rossamitchell2609@yahoo.co.uk>
2022-10-04 02:59:31 +0100

    Basic Vue Setup

---
+++ resources/js/Pages/Gallery.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Gallery</h1>
+
+    <Nav />
+</template>
+
+<script>
+    import Nav from '../Shared/Nav';
+
+    export default {
+        props: {},
+        components: { Nav }
+    };
+</script>
---
+++ resources/js/Shared/Nav.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav>
+        <ul>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/gallery">Gallery</Link></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+    import { Link } from '@inertiajs/inertia-vue3';
+
+    export default {
+        components: { Link }
+    };
+</script>
