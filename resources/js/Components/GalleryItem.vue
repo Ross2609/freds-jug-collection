fb711833cd1ffa14876b1010052170be23e6ac5f
Ross Mitchell <rossamitchell2609@gmail.com>
2022-10-15 18:32:23 +0100

    Install new font

--- resources/js/Components/GalleryItem.vue
+++ resources/js/Components/GalleryItem.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="border rounded-lg my-3 flex flex-col place-items-center">
+    <div class="border rounded-lg my-3 flex flex-col place-items-center shadow-lg">
         <img :src="item.img_url" :alt="item.img_name" class="w-44 m-2 border border-gray-200 object-cover border rounded mt-4">
-        <p class="mt-5 text-center mb-3" style="font-family: Verdana">{{ item.name }}</p>
+        <p class="mt-5 text-center mb-3">{{ item.name }}</p>
     </div>
 </template>
 
