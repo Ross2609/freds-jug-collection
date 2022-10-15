5838c3fa37cdf86fae18effd20f804f18d139d55
Ross Mitchell <rossamitchell2609@gmail.com>
2022-10-15 18:35:16 +0100

    Publish public folder

--- public/js/app.js
+++ public/js/app.js
@@ -15380,14 +15380,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
 var _hoisted_1 = {
-  "class": "border rounded-lg my-3 flex flex-col place-items-center"
+  "class": "border rounded-lg my-3 flex flex-col place-items-center shadow-lg"
 };
 var _hoisted_2 = ["src", "alt"];
 var _hoisted_3 = {
-  "class": "mt-5 text-center mb-3",
-  style: {
-    "font-family": "Verdana"
-  }
+  "class": "mt-5 text-center mb-3"
 };
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_1, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("img", {
